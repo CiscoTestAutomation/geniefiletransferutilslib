b8cd922c954bc5579190d3aae60ed1be1b8ae8bd
Myles Dear <mdear@cisco.com>
2018-03-19 19:18:29 -0400

    Removed pdb which was causing import to fai

--- src/filetransferutils/fileutils.py
+++ src/filetransferutils/fileutils.py
@@ -1,5 +1,4 @@
 """ File utils base class for filetransferutils package. """
-import pdb; pdb.set_trace()
 from ats.utils.fileutils import FileUtils as FileUtilsBase
 
 class FileUtils(FileUtilsBase):
