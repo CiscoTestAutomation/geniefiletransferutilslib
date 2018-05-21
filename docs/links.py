513bd05502b84120a096ad0c736a24528fbcfe55
karmoham <karmoham@cisco.com>
2018-05-21 14:45:45 -0400

    Adding 'unicon' package dependency + CesMonitor

--- docs/links.py
+++ docs/links.py
@@ -6,9 +6,9 @@ internal_links = {'pyats': ('%s://wwwin-pyats.cisco.com', 'pyATS'),
                   'communityforum': ('%s://piestack.cisco.com', 'community forum'),
                   }
 
-external_links = {'pyats': ('%s://developer.cisco.com/site/pyats/', 'pyATS'),
-                  'devnet': ('%s://developer.cisco.com/', 'Cisco DevNet'),
-                  'MultiprotocolUtilities': ('%s://developer.cisco.com/site/pyats/', 'Multiprotocol File Transfer'),
+external_links = {'pyats': ('%ss://developer.cisco.com/site/pyats/', 'pyATS'),
+                  'devnet': ('%ss://developer.cisco.com/', 'Cisco DevNet'),
+                  'MultiprotocolUtilities': ('%ss://pubhub.devnetcloud.com/media/pyats/docs/utilities/file_transfer_utilities.html', 'Multiprotocol File Transfer'),
                   'mailto': ('mailto:pyats-support-ext@%s','mailto'),
-                  'communityforum': ('%s:%s://communities.cisco.com/community/developer/pyats', 'community forum'),
+                  'communityforum': ('%ss://communities.cisco.com/community/developer/pyats', 'community forum'),
                   }
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ URL = _EXTERNAL_URL if is_devnet_build() else _INTERNAL_URL
 version, version_range = version_info('src', 'genie', 'libs', 'filetransferutils', '__init__.py')
 
 # generate package dependencies
-install_requires = []
+install_requires=['unicon']
 
 # launch setup
 setup(
